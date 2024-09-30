ceca95fbd0350eddda190c9c3369951939c7fee9
Danil Sherbakov <dandiaborn@gmail.com>
2024-09-30 13:15:26 +0300

    dashboard fetch

--- resources/views/dashboard/consultation/item.blade.php
+++ resources/views/dashboard/consultation/item.blade.php
@@ -17,135 +17,109 @@
         <div class="main__wrapper">
           <h1 class="main__title">Консультация №{{ $consultation->id }}</h1>
 
-          <section class="main__pages pages">
-            <div class="pages__wrapper">
+          <section class="main__consultation consultation">
+            <div class="consultation__wrapper white-block">
               <h2 class="pages__title">{{ $consultation->title }}</h2>
               <div class="pages__inner">
-                  <div class="pages__item">
-					{{ $consultation->description }}
+                <div class="pages__item">
+                  {{ $consultation->description }}
+                </div>
+                <span class="pages__views-id">{{ $consultation->created_at }}</span>
+                <div class="pages__icons">
+                  <div class="pages__icon">
+                    <a href="{{ route('dashboard.consultation.edit', $consultation->id)}}" target="_blank">
+                      <img src="/images/dashboard/edit.svg" alt="" class="pages__icon-img">
+                    </a>
                   </div>
-				  
-				  <span class="pages__views-id">{{ $consultation->created_at }}</span>
-                    <div class="pages__icons">
-                      <div class="pages__icon">
-                        <a href="{{ route('dashboard.consultation.edit', $consultation->id)}}" target="_blank">
-                          <img src="/images/dashboard/edit.svg" alt="" class="pages__icon-img">
-                        </a>
-                      </div>
-                      <div class="pages__icon">
-                        <a href="{{ route('dashboard.consultation.destroy', $consultation->id) }}">
-                          <img src="/images/dashboard/del.svg" alt="" class="pages__icon-img">
-                        </a>
-                      </div>
-                    </div>
+                  <div class="pages__icon">
+                    <a href="{{ route('dashboard.consultation.destroy', $consultation->id) }}">
+                      <img src="/images/dashboard/del.svg" alt="" class="pages__icon-img">
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
-          </section> 
-		  
-		   <br /><br />
-		   
-		    <section class="main__pages booking">
-            <div class="pages__wrapper">
-				<div style="display:flex; border-radius: 24px;justify-content: space-between;background-color: #f2f3f5;">
-					<div style="box-sizing: border-box;width: 100%;">Чтобы ответить, нажмите взять вопрос</div>
-					<div style="max-width: 254px;"><button style="background-color: #ef3124;
-    color: hsla(0,0%,100%,.94);align-content: center;
-    align-items: center;
-    background-color: transparent;
-    border: 0;
-    border-radius: 8px;
-    box-shadow: none;
-    box-sizing: border-box;
-    cursor: pointer;
-    display: inline-flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    font-family: system-ui,-apple-system,Segoe UI,Roboto,Helvetica Neue,Helvetica,sans-serif;
-    font-weight: 500;
-    justify-content: center;
-    line-height: 20px;
-    margin: 0;
-    outline: 0;
-    padding: 0 24px;
-    position: relative;
-    text-align: center;
-    text-decoration: none;
-    transition: background .2s ease,border .2s ease,color .2s ease,transform .12s ease;
-    -webkit-user-select: none;
-    user-select: none;
-    vertical-align: middle;
-    will-change: transform;" type="submit" onclick="makeBooking()"><span class="button__text_1mgd7 button__stretchText_1mgd7">Продолжить</span></button></div></div>
-            </div>
           </section>
-		    <br /><br />
-			
-		   <section class="main__pages pages">
-            <div class="pages__wrapper">
-				<form action="" method="POST">
-				
-				 @csrf
-					<textarea style="width:100%;height:150px"></textarea>
-					<input type="submit" value="Отправить">
-				</form>
-            </div>
-          </section>
-		  <br /><br />
-		  
-		   @foreach($consultation->comments as $comment)
-                <div style="display:flex;margin-bottom: 40px;padding:15px;border-radius: 8px;background:#fff">
-                <div> {{ $comment->description }}</div>
-                <div> Ответить</div>
-                <div> Удалить</div>
-                </div>
-				@include('dashboard.consultation.childcomment', ['comments' => $comment->children])
-            @endforeach
-			
-			<script>
 
-	const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-	async function makeBooking() {
-		
-		
-	const data = {
-            'id': {{ $consultation->id }}
-        };
-		
-        try {
-			
-		
-            const response = await fetch(`{{ route('dashboard.consultation.booking', $consultation->id) }}`, {
+          <section class="main__booking booking">
+            <div class="booking__wrapper white-block">
+              <div class="booking__text">Чтобы ответить, нажмите взять вопрос</div>
+              <div class=" booking__button" onclick="makeBooking()">Войти</div>
+          </section>
+
+          <section class="main__pages pages">
+            <div class="pages__wrapper">
+              <form action="" method="POST">
+
+                @csrf
+                <textarea style="width:100%;height:150px"></textarea>
+                <input type="submit" value="Отправить">
+              </form>
+            </div>
+          </section>
+          <br /><br />
+
+          @foreach($consultation->comments as $comment)
+          <div style="display:flex;margin-bottom: 40px;padding:15px;border-radius: 8px;background:#fff">
+            <div> {{ $comment->description }}</div>
+            <div> Ответить</div>
+            <div> Удалить</div>
+          </div>
+          @include('dashboard.consultation.childcomment', ['comments' => $comment->children])
+          @endforeach
+
+          <script>
+          const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+          const url = window.location.href; // Получаем полный URL
+          const segments = url.split('/'); // Разбиваем строку URL по символу '/'
+          const consultationId = segments[segments.length - 1]; // Получаем последний сегмент (ID)
+          async function makeBooking() {
+
+
+            try {
+              const response = await fetch(`{{ route('dashboard.consultation.booking', $consultation->id) }}`, {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': csrfToken
+                  'Content-Type': 'application/json',
+                  'X-CSRF-TOKEN': csrfToken
                 },
-                body: {{ $consultation->id }}
-            });
+                body: JSON.stringify({
+                  'consultation_id': consultationId,
+                  'user_id': 1
+                })
+              });
 
-        // Проверка успешности ответа
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
 
-        // Получение данных из ответа (в формате JSON)
-        const data = await response.json();
+              // Проверка успешности ответа
+              if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+              }
 
-        // Предполагаем, что мы ожидаем, что объект данных будет содержать поле 'success'
-        if (data.success) {
-			console.log(data.message);
-           
-        } else {
-            console.log("Запрос выполнен, но не success.");
-        }
-    } catch (error) {
-        console.error("Ошибка при выполнении запрос: ", error);
-    }
-}
-	
-</script>
-		 
-		  
-		 
+              // Получение данных из ответа (в формате JSON)
+              const data = await response.json();
+
+              // Предполагаем, что мы ожидаем, что объект данных будет содержать поле 'success'
+              if (data.success) {
+                console.log(data.message);
+                if (data.message) {
+                  document.querySelector('.booking__text').innerHTML = data.message;
+                } else {
+                  document.querySelector('.booking__text').innerHTML = 'Произошла ошибка';
+                }
+                document.querySelector('.booking__button').style.display = 'none';
+              } else {
+                console.log("Запрос выполнен, но не success.");
+              }
+            } catch (error) {
+              console.error("Ошибка при выполнении запрос: ", error);
+            }
+          }
+          console.log(makeBooking);
+          </script>
+
+
+
         </div>
       </main>
     </div>
