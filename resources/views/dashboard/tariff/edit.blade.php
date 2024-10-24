c9e8208dfe5a7c3a2eb797bd5e82eba879fca737
Danil Sherbakov <dandiaborn@gmail.com>
2024-10-24 17:26:51 +0300

    tariff edir fixes

--- resources/views/dashboard/tariff/edit.blade.php
+++ resources/views/dashboard/tariff/edit.blade.php
@@ -37,58 +37,34 @@
                           <label class="form__label" for="name">Название</label>
                           <input class="form__input @error('name')input-error @enderror" type="text" id="name"
                             name="name" value="{{ $tariff->name }}">
-                          <div class="form__input-wrapper-row">
-                            <input class="form__input" type="checkbox" id="name[is_empty]" name="name[is_empty]">
-                            <label class="form__label" for="name[is_empty]">Пустой</label>
-                          </div>
                         </li>
                         <li class="form__input-wrapper">
                           <label class="form__label" for="title">Название</label>
                           <input class="form__input @error('title')input-error @enderror" type="text" id="title"
                             name="title" value="{{ $tariff->title }}">
-                          <div class="form__input-wrapper-row">
-                            <input class="form__input" type="checkbox" id="title[is_empty]" name="title[is_empty]">
-                            <label class="form__label" for="title[is_empty]">Пустой</label>
-                          </div>
+
                         </li>
                         <li class="form__input-wrapper">
                           <label class="form__label" for="description">Описание</label>
-                          <input class="form__input @error('description')input-error @enderror" type="text"
-                            id="description" name="description" value="{{ $tariff->description }}">
-                          <div class="form__input-wrapper-row">
-                            <input class="form__input" type="checkbox" id="description[is_empty]"
-                              name="description[is_empty]">
-                            <label class="form__label" for="description[is_empty]">Пустой</label>
-                          </div>
+                          <textarea class="form__input @error('description')input-error @enderror" type="text"
+                            id="description" name="description">{{ $tariff->description }}</textarea>
                         </li>
                         <li class="form__input-wrapper">
                           <label class="form__label" for="description_short">Краткое описание</label>
-                          <input class="form__input @error('description_short')input-error @enderror" type="text"
-                            id="description_short" name="description_short" value="{{ $tariff->description_short }}">
-                          <div class="form__input-wrapper-row">
-                            <input class="form__input" type="checkbox" id="description_short[is_empty]"
-                              name="description_short[is_empty]">
-                            <label class="form__label" for="description_short[is_empty]">Пустой</label>
-                          </div>
+                          <textarea class="form__input @error('description_short')input-error @enderror" type="text"
+                            id="description_short" name="description_short">{{ $tariff->description_short }}</textarea>
                         </li>
                         <li class="form__input-wrapper">
                           <label class="form__label" for="answers_count">Количество ответов</label>
                           <input class="form__input @error('answers_count')input-error @enderror" type="text"
                             id="answers_count" name="answers_count" value="{{ $tariff->answers_count }}">
-                          <div class="form__input-wrapper-row">
-                            <input class="form__input" type="checkbox" id="answers_count[is_empty]"
-                              name="answers_count[is_empty]">
-                            <label class="form__label" for="answers_count[is_empty]">Пустой</label>
-                          </div>
+
                         </li>
                         <li class="form__input-wrapper">
                           <label class="form__label" for="sum">Сумма</label>
                           <input class="form__input @error('sum')input-error @enderror" type="text" id="sum" name="sum"
                             value="{{ $tariff->sum }}">
-                          <div class="form__input-wrapper-row">
-                            <input class="form__input" type="checkbox" id="sum[is_empty]" name="sum[is_empty]">
-                            <label class="form__label" for="sum[is_empty]">Пустой</label>
-                          </div>
+
                         </li>
                         <li class="form__input-wrapper">
                           <span class="form__select-title" for="condition_id">Состояние</span>
@@ -120,94 +96,29 @@
                           <label class="form__label" for="position">Позиция</label>
                           <input class="form__input @error('position')input-error @enderror" type="text" id="position"
                             name="position" value="{{ $tariff->position }}">
+                        </li>
+                        <li class="form__input-wrapper">
                           <div class="form__input-wrapper-row">
-                            <input class="form__input" type="checkbox" id="position[is_empty]"
-                              name="position[is_empty]">
-                            <label class="form__label" for="position[is_empty]">Пустой</label>
+                            <input class="form__input" type="checkbox" id="is_phone" name="is_phone">
+                            <label class="form__label" for="is_phone">Телефон</label>
                           </div>
                         </li>
                         <li class="form__input-wrapper">
-                          <span class="form__select-title" for="is_phone">Телефон</span>
-                          <div class="form__select-wrapper form__select-wrapper-standart">
-                            <img src="/images/dashboard/expand-more.svg" alt="" class="form__status-arrow">
-                            <div class="form__status-current-text normal-text">Выберите состояние</div>
-                            <input class="form__status-current " name="is_phone" type="text"
-                              value="{{ $tariff->is_phone }}" readonly>
-                            <ul id="is_phone"
-                              class="form__status-select form__status-select-standart  form__status-hide">
-                              <li class="form__status-option" value="">
-                                Да или нет
-                              </li>
-                              <li class="form__status-option" value="1">
-                                Да
-                              </li>
-                              <li class="form__status-option" value="2">
-                                Нет
-                              </li>
-                            </ul>
+                          <div class="form__input-wrapper-row">
+                            <input class="form__input" type="checkbox" id="is_free" name="is_free">
+                            <label class="form__label" for="is_free">Свободен</label>
                           </div>
                         </li>
                         <li class="form__input-wrapper">
-                          <span class="form__select-title" for="is_free">Свободен</span>
-                          <div class="form__select-wrapper form__select-wrapper-standart">
-                            <img src="/images/dashboard/expand-more.svg" alt="" class="form__status-arrow">
-                            <div class="form__status-current-text normal-text">Выберите состояние</div>
-                            <input class="form__status-current " name="is_free" type="text"
-                              value="{{ $tariff->is_free }}" readonly>
-                            <ul id="is_free"
-                              class="form__status-select form__status-select-standart  form__status-hide">
-                              <li class="form__status-option" value="">
-                                Да или нет
-                              </li>
-                              <li class="form__status-option" value="1">
-                                Да
-                              </li>
-                              <li class="form__status-option" value="2">
-                                Нет
-                              </li>
-                            </ul>
+                          <div class="form__input-wrapper-row">
+                            <input class="form__input" type="checkbox" id="condition_checked" name="condition_checked">
+                            <label class="form__label" for="condition_checked">Проверено состояние</label>
                           </div>
                         </li>
                         <li class="form__input-wrapper">
-                          <span class="form__select-title" for="condition_checked">Проверено состояние</span>
-                          <div class="form__select-wrapper form__select-wrapper-standart">
-                            <img src="/images/dashboard/expand-more.svg" alt="" class="form__status-arrow">
-                            <div class="form__status-current-text normal-text">Выберите состояние</div>
-                            <input class="form__status-current " name="condition_checked" type="text"
-                              value="{{ $tariff->condition_checked }}" readonly>
-                            <ul id="condition_checked"
-                              class="form__status-select form__status-select-standart  form__status-hide">
-                              <li class="form__status-option" value="">
-                                Да или нет
-                              </li>
-                              <li class="form__status-option" value="1">
-                                Да
-                              </li>
-                              <li class="form__status-option" value="2">
-                                Нет
-                              </li>
-                            </ul>
-                          </div>
-                        </li>
-                        <li class="form__input-wrapper">
-                          <span class="form__select-title" for="condition_active">Активний</span>
-                          <div class="form__select-wrapper form__select-wrapper-standart">
-                            <img src="/images/dashboard/expand-more.svg" alt="" class="form__status-arrow">
-                            <div class="form__status-current-text normal-text">Выберите состояние</div>
-                            <input class="form__status-current " name="condition_active" type="text"
-                              value="{{ $tariff->condition_active }}" readonly>
-                            <ul id="condition_active"
-                              class="form__status-select form__status-select-standart  form__status-hide">
-                              <li class="form__status-option" value="">
-                                Да или нет
-                              </li>
-                              <li class="form__status-option" value="1">
-                                Да
-                              </li>
-                              <li class="form__status-option" value="2">
-                                Нет
-                              </li>
-                            </ul>
+                          <div class="form__input-wrapper-row">
+                            <input class="form__input" type="checkbox" id="condition_active" name="condition_active">
+                            <label class="form__label" for="condition_active">Активный</label>
                           </div>
                         </li>
                         <li class="form__input-wrapper">
@@ -247,6 +158,12 @@
                                     задать вопрос врачу</li>
                                   <li class="custom-multiselect__option" value="10">Консультация кардиолога онлайн —
                                     задать вопрос врачу</li>
+                                  <li class="custom-multiselect__option" value="11">Консультация кардиолога онлайн —
+                                    задать вопрос врачу</li>
+                                  <li class="custom-multiselect__option" value="12">Консультация кардиолога онлайн —
+                                    задать вопрос врачу</li>
+                                  <li class="custom-multiselect__option" value="13">Консультация кардиолога онлайн —
+                                    задать вопрос врачу</li>
                                 </ul>
                               </div>
                             </div>
@@ -273,7 +190,7 @@
                   </div>
                 </div>
                 <div class="form__textarea-wrapper">
-                  <input class="form__submit" type="submit"> </input>
+                  <input class="form__submit red-button" type="submit"> </input>
                 </div>
               </form>
             </div>
