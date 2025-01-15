8be070c4e67cc407065314c99863f64553a14f46
Danil Sherbakov <dandiaborn@gmail.com>
2025-01-15 11:28:53 +0300

    chat

--- resources/views/consultation/category/index.blade.php
+++ resources/views/consultation/category/index.blade.php
@@ -278,8 +278,18 @@
         <div class="chat__message">Здравствуйте. Опишите свою проблему. </div>
         <div class="chat__textarea-wrapper">
           <span class="chat__title">Ваш вопрос</span>
-          <textarea name="" id="" class="chat__question"
-            placeholder="- опишите вашу ситуацию;&#10- сформулируйте вопрос."></textarea>
+          <div class="chat__question-wrapper">
+            <textarea name="" id="" class="chat__question"
+              placeholder="- опишите вашу ситуацию;&#10- сформулируйте вопрос."></textarea>
+            <div class="chat__button-continue" id="button-next">
+              <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"
+                class="icon-2xl">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M15.1918 8.90615C15.6381 8.45983 16.3618 8.45983 16.8081 8.90615L21.9509 14.049C22.3972 14.4953 22.3972 15.2189 21.9509 15.6652C21.5046 16.1116 20.781 16.1116 20.3347 15.6652L17.1428 12.4734V22.2857C17.1428 22.9169 16.6311 23.4286 15.9999 23.4286C15.3688 23.4286 14.8571 22.9169 14.8571 22.2857V12.4734L11.6652 15.6652C11.2189 16.1116 10.4953 16.1116 10.049 15.6652C9.60265 15.2189 9.60265 14.4953 10.049 14.049L15.1918 8.90615Z"
+                  fill="currentColor"></path>
+              </svg>
+            </div>
+          </div>
         </div>
       </div>
       <div class="chat__additional-fields hide">
@@ -297,9 +307,6 @@
         </div>
       </div>
       <div class="chat__footer">
-        <div class="chat__footer-first">
-          <div class="chat__button-next" id="button-next">Дальше</div>
-        </div>
         <div class="chat__footer-second hide">
           <div class="chat__button-prev" id="button-prev">Назад</div>
           <div class="chat__button-next" id="button-submit"> Отправить</div>
