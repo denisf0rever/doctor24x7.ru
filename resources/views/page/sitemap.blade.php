d860a4a65d00392e68fffa12e037bdf55a2e2310
DanSher25 <dansher250@gmail.com>
2025-04-21 12:51:15 +0300

    sitemap

--- resources/views/page/sitemap.blade.php
+++ resources/views/page/sitemap.blade.php
@@ -6,4 +6,21 @@
 
 @section('content')
 
+<section class="sitemap">
+  <div class="sitemap__wrapper container">
+    <h3 class="sitemap__title">Lorem ipsum dolor </h3>
+    <div class="sitemap__list">
+      <a href="/" class="sitemap__item">Lorem ipsum dolor</a>
+      <a href="/" class="sitemap__item">Lorem ipsum dolor</a>
+      <a href="/" class="sitemap__item">Lorem ipsum dolor</a>
+      <a href="/" class="sitemap__item">Lorem ipsum dolor</a>
+      <a href="/" class="sitemap__item">Lorem ipsum dolor</a>
+      <a href="/" class="sitemap__item">Lorem ipsum dolor</a>
+      <a href="/" class="sitemap__item">Lorem ipsum dolor</a>
+      <a href="/" class="sitemap__item">Lorem ipsum dolor</a>
+      <a href="/" class="sitemap__item">Lorem ipsum dolor</a>
+    </div>
+  </div>
+</section>
+
 @endsection
