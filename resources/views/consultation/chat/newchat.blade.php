2e590755094673b0b7ce33841d46c21939f977f3
DanSher25 <dansher250@gmail.com>
2025-05-16 13:20:04 +0300

    payment email input

--- resources/views/consultation/chat/newchat.blade.php
+++ resources/views/consultation/chat/newchat.blade.php
@@ -5,12 +5,13 @@
 <section class="main__receipt receipt">
   <div class="receipt__wrapper">
     <form action="{{ route('chat.create') }}" method="post" class="receipt__form">
-		@csrf
+      @csrf
       <img src="https://img.freepik.com/free-vector/hand-drawn-clothes-person_79603-614.jpg?semt=ais_hybrid&w=740"
         alt="" class="payment-method__avatar">
       <span class="payment-method__big-name">ИМЯ</span>
-      <span class="payment-method__subtitle">Подзаголовок</span>
-      <input type="text" name="email" class="payment-method__subtitle" value="">
+      <div class="payment-method__email-input-wrapper">
+        <input type="email" name="email" class="payment-method__email-input" placeholder="Введите свою почту" value="">
+      </div>
       <div class="payment-method__submit-wrapper">
         <span class="payment-method__submit-text">Далее будет создан чат для консультации</span>
         <input type="submit" value="Начать чат" class="payment-method__submit">
