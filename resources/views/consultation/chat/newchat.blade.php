8ba2aa6ba9fbd3d29b3c4a3ad7073615546b6f00
DanSher25 <dansher250@gmail.com>
2025-05-21 11:52:43 +0300

    chat/consultant table

--- resources/views/consultation/chat/newchat.blade.php
+++ resources/views/consultation/chat/newchat.blade.php
@@ -9,6 +9,20 @@
       <img src="https://img.freepik.com/free-vector/hand-drawn-clothes-person_79603-614.jpg?semt=ais_hybrid&w=740"
         alt="" class="payment-method__avatar">
       <span class="payment-method__big-name">ИМЯ</span>
+      <div class="payment-method__table">
+        <div class="payment-method__table-row">
+          <span class="payment-method__table-cell">Вариант ответа</span>
+          <span class="payment-method__table-cell">Вариант ответа</span>
+        </div>
+        <div class="payment-method__table-row">
+          <span class="payment-method__table-cell">Вариант ответа</span>
+          <span class="payment-method__table-cell">123</span>
+        </div>
+        <div class="payment-method__table-row">
+          <span class="payment-method__table-cell">Lorem ipsum, dolor sit amet consectetur</span>
+          <span class="payment-method__table-cell">123</span>
+        </div>
+      </div>
       <div class="payment-method__email-input-wrapper">
         <input type="hidden" name="consultant_id" value="{{ $user->id }}">
         <input type="email" name="email" class="payment-method__email-input" placeholder="Введите свою почту" value="">
