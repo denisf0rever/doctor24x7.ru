49d33002b0885b2d293d5a92246d5e157a6b8610
DanSher25 <dansher250@gmail.com>
2025-06-28 13:10:56 +0300

    minor fixes

--- resources/views/page/sitemap.blade.php
+++ resources/views/page/sitemap.blade.php
@@ -7,19 +7,24 @@
 @section('content')
 <section class="sitemap">
   <div class="sitemap__wrapper container">
-    <h1 class="sitemap__title">Консультации</h1>
-    <div class="sitemap__list">
-      @foreach ($categories as $category)
-		<a href="{{ route('consultation.category', $category->slug) }}" class="sitemap__item">{{ $category->short_title }}</a>
-	@endforeach
+    <div class="sitemap__section">
+      <h1 class="sitemap__title">Консультации</h1>
+      <div class="sitemap__list">
+        @foreach ($categories as $category)
+        <a href="{{ route('consultation.category', $category->slug) }}"
+          class="sitemap__item">{{ $category->short_title }}</a>
+        @endforeach
+      </div>
     </div>
-	
-	<h1 class="sitemap__title">Форум</h1>
-    <div class="sitemap__list">
-      @foreach ($forums as $forum)
-		<a href="{{ route('forum.category', $forum->slug) }}" class="sitemap__item">{{ $forum->short_title }}</a>
-	@endforeach
+    <div class="sitemap__section">
+      <h1 class="sitemap__title">Форум</h1>
+      <div class="sitemap__list">
+        @foreach ($forums as $forum)
+        <a href="{{ route('forum.category', $forum->slug) }}" class="sitemap__item">{{ $forum->short_title }}</a>
+        @endforeach
+      </div>
     </div>
+
   </div>
 </section>
 
