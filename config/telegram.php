24c7fa9c82d3f49767d70c659f6d120166f8947c
Denis Kadyrgulov <predlozhi@bk.ru>
2025-07-05 13:44:45 +0300

    Removed config/config.php from Git and added to .gitignore

--- config/telegram.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-       'telegram_admin_id' => '108494029',
-	   'consultation_token' => '5532676446:AAEfcknO5o71mfYqKfKlv1VLdX_4YoLOqZw', // Консультации в работе
-       'token' => '7897419260:AAFoE-bT273-qj554aS6TuWhowb1Xw2pejE',
-       'CallBackNotifierToken' => '8064234333:AAH3QmAlA5h9gnfGueCKxghELex5T2vdz7M',
-   ]; 
