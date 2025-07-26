ba4d9a819c712c5bd2b27345eabd90c9391e0118
DanSher25 <dansher250@gmail.com>
2025-07-26 13:58:34 +0300

    balance form fix

--- resources/views/account/balance.blade.php
+++ resources/views/account/balance.blade.php
@@ -14,43 +14,45 @@
         <span class="balance__subtitle">Свободных стредств</span>
         <span class="balance__amount">{{ $account->balance }} ₽</span>
         <form action="{{ route('payment.init') }}" class="balance__add-balance-form" method="post">
-		@csrf
-		
-		@if ($errors->any())
-            <ul>
-              @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-              @endforeach
-            </ul>
-            @endif
-			<input type="hidden" name="payment_purpose" value="balance_account">
-			<input type="hidden" name="OrderId" value="{{ auth()->id() }}">
+          @csrf
+
+          @if ($errors->any())
+          <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+          @endif
+          <input type="hidden" name="payment_purpose" value="balance_account">
+          <input type="hidden" name="OrderId" value="{{ auth()->id() }}">
+          <input type="hidden" name="Sum" value="{{ auth()->id() }}" id="balance__amount-hidden">
           <div class="balance__add-balance-label">
             <span class="balance__amount-span">Сумма пополнения</span>
-            <input type="text" name="Sum" class="balance__amount-input" placeholder="1 000 ₽" value="">
+            <input type="text" name="inputSum" class="balance__amount-input" placeholder="1 000 ₽" value="">
           </div>
           <!--<button type="submit" class="balance__add-balance-button new-red-button">Пополнить</button>-->
-       
-        <div class="balance__method payment-method">
-          <h1 class="payment__normal-title">Способ оплаты</h1>
-          <div class="payment-method__list">
 
-            <div class="payment-method__item">
-              <input type="radio" name="payment_method" class="payment-method__input" value="t_bank" id="payment_method" checked>
-              <label for="payment_method" class="payment-method__label">
-                <div class="payment-method__icon">
-                  <img src="{{ Storage::url('payment/TPay.svg') }}">
-                </div>
-                <div class="payment-method__body">
-                  <span class="payment-method__title">Т-банк касса</span>
-                  <span class="payment-method__text">Карта любого банка, SBpay</span>
-                </div>
-              </label>
+          <div class="balance__method payment-method">
+            <h1 class="payment__normal-title">Способ оплаты</h1>
+            <div class="payment-method__list">
+
+              <div class="payment-method__item">
+                <input type="radio" name="payment_method" class="payment-method__input" value="t_bank"
+                  id="payment_method" checked>
+                <label for="payment_method" class="payment-method__label">
+                  <div class="payment-method__icon">
+                    <img src="{{ Storage::url('payment/TPay.svg') }}">
+                  </div>
+                  <div class="payment-method__body">
+                    <span class="payment-method__title">Т-банк касса</span>
+                    <span class="payment-method__text">Карта любого банка, SBpay</span>
+                  </div>
+                </label>
+              </div>
             </div>
+            <input type="submit" value="Пополнить баланс" class="payment-method__submit">
           </div>
-          <input type="submit" value="Пополнить баланс" class="payment-method__submit">
-        </div>
-		</form>
+        </form>
       </div>
     </div>
   </section>
